1a6f3bdcf7344c89f48d50b85bbd4b571d0ec4c9
Matt Smith <msmith@ziji-media.com>
2014-07-22 22:35:47 -0600

    Clean up invalid messages

--- src/main/java/com/charter/aesd/aws/snsclient/demo/QueuePOC.java
+++ src/main/java/com/charter/aesd/aws/snsclient/demo/QueuePOC.java
@@ -174,6 +174,9 @@ public class QueuePOC {
                     }
 
                     String msg = _sqsClient.receiveMessage(_queueUrl);
+                    if (msg == null) {
+                        continue;
+                    }
 
                     process(msg);
                 } catch(Exception e) {

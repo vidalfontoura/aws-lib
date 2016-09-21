55b435dfa662da62c96f127ed5c6351a4cd72ebd
Chadwick Boggs <Chadwick.Boggs@charter.com>
2016-09-21 15:24:36 -0600

    USRVS-4472 Completed EC2ClientStub.

--- src/main/java/com/charter/aesd/aws/ec2/client/api/impl/EC2ClientStub.java
+++ src/main/java/com/charter/aesd/aws/ec2/client/api/impl/EC2ClientStub.java
@@ -6,31 +6,23 @@ package com.charter.aesd.aws.ec2.client.api.impl;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.regions.Region;
 import com.amazonaws.services.ec2.AmazonEC2Client;
-import com.amazonaws.services.ec2.model.AuthorizeSecurityGroupEgressRequest;
-import com.amazonaws.services.ec2.model.AuthorizeSecurityGroupIngressRequest;
-import com.amazonaws.services.ec2.model.CreateSecurityGroupRequest;
 import com.amazonaws.services.ec2.model.CreateSecurityGroupResult;
 import com.amazonaws.services.ec2.model.DescribeVpcsRequest;
 import com.amazonaws.services.ec2.model.DescribeVpcsResult;
 import com.amazonaws.services.ec2.model.IpPermission;
-import com.amazonaws.services.ec2.model.RevokeSecurityGroupEgressRequest;
-import com.amazonaws.services.ec2.model.RevokeSecurityGroupIngressRequest;
 import com.amazonaws.services.ec2.model.SecurityGroup;
 import com.amazonaws.services.ec2.model.UserIdGroupPair;
 import com.amazonaws.services.ec2.model.Vpc;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.charter.aesd.aws.ec2.client.api.EC2Client;
-import com.charter.aesd.aws.ec2.command.EC2Command;
 import com.charter.aesd.aws.ec2.securitygroup.SecurityGroupQuery;
 import com.charter.aesd.aws.enums.AWSAuthType;
 import com.charter.aesd.aws.s3client.S3Client;
+import org.apache.commons.collections4.CollectionUtils;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import rx.Observable;
-import rx.schedulers.Schedulers;
 
-import java.lang.invoke.MethodHandles;
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -38,108 +30,138 @@ import java.util.function.Supplier;
 
 
 /**
- * AWS client for accessing EC2 resources within the AWS EC2 API<br />
- * Use {@link EC2ClientStub.Builder} to construct an instance of
- * {@link EC2Client}
- *
- * @author jappel
+ * A stub AWS client for accessing EC2 resources within the AWS EC2 API.
  */
 public class EC2ClientStub implements EC2Client {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().getClass());
-
     /**
      * Private method to invoke {@code Hystrix} command for each AWS EC2 APIO
-     * 
+     *
      * @param function
      * @return
      */
     private <T> Observable<T> invokeHystrixCommand(Supplier<T> function) {
 
-        EC2Command<T> command = new EC2Command<T>(function);
-        try {
-            return command.observe().subscribeOn(Schedulers.io());
-        } catch (Exception e) {
-            LOGGER.error("Error executing AWS EC2 command", e);
-        }
-        return Observable.empty();
+        throw new UnsupportedOperationException();
     }
 
+
+    /**
+     * Returns new mock groups corresponding to queried group identifiers and names.
+     *
+     * @param query the query.
+     * @return new mock groups corresponding to queried group identifiers and names.
+     */
     @Override
     public Observable<SecurityGroup> describeSecurityGroups(final Optional<SecurityGroupQuery> query) {
 
-        Supplier<List<SecurityGroup>> function =
-            () -> {
-                List<SecurityGroup> securityGroups = new ArrayList<>();
+        if (!query.isPresent()) {
+            return Observable.empty();
+        }
 
-                List<String> groupIds = query.get().getGroupIds();
-                List<String> groupNames = query.get().getGroupNames();
-                for (int i = 0; i < groupIds.size(); i++) {
-                    String groupId = groupIds.get(i);
-                    String groupName = groupNames.get(i);
-                    SecurityGroup securityGroup = Mockito.mock(SecurityGroup.class);
-                    securityGroups.add(securityGroup);
-                    Mockito.when(securityGroup.getGroupId()).thenReturn(groupId);
-                    Mockito.when(securityGroup.getGroupName()).thenReturn(groupName);
-                }
+        SecurityGroupQuery securityGroupQuery = query.get();
+        List<String> groupIds = securityGroupQuery.getGroupIds();
+        if (CollectionUtils.isEmpty(groupIds)) {
+            return Observable.empty();
+        }
 
-                return securityGroups;
-            };
-        return invokeHystrixCommand(function).flatMap(lst -> Observable.from(lst));
+        List<SecurityGroup> securityGroups = new ArrayList<>();
+        List<String> groupNames = securityGroupQuery.getGroupNames();
+
+        for (int i = 0; i < groupIds.size(); i++) {
+            SecurityGroup securityGroup = createSecurityGroup(groupIds.get(i));
+            securityGroups.add(securityGroup);
+
+            if (CollectionUtils.isEmpty(groupNames) || groupNames.size() <= i) {
+                continue;
+            }
+
+            Mockito.when(securityGroup.getGroupName()).thenReturn(groupNames.get(i));
+        }
+
+        return Observable.from(securityGroups);
     }
 
+
+    private SecurityGroup createSecurityGroup(@Nonnull final String groupId) {
+        SecurityGroup securityGroup = Mockito.mock(SecurityGroup.class);
+        Mockito.when(securityGroup.getGroupId()).thenReturn(groupId);
+        Mockito.when(securityGroup.getIpPermissions()).thenReturn(createIpPermissions());
+        Mockito.when(securityGroup.getIpPermissionsEgress()).thenReturn(createIpPermissions());
+
+        return securityGroup;
+    }
+
+
+    private List<IpPermission> createIpPermissions() {
+        List<IpPermission> ipPermissions = new ArrayList<>();
+        IpPermission ipPermission = new IpPermission()
+            .withIpProtocol("tcp")
+            .withIpRanges("0.0.0.0/0")
+            .withFromPort(0)
+            .withToPort(65535);
+        ipPermissions.add(ipPermission);
+
+        return ipPermissions;
+    }
+
+
+    /**
+     * Returns a new mock group with group identifier "1".
+     *
+     * @param groupName        the name of the group that should be created.
+     * @param vpcId            the VPC in which the security group should be created.
+     * @param groupDescription the description of the group that should be created.
+     * @returns a new mock group with group identifier "1".
+     */
     @Override
     public Observable<CreateSecurityGroupResult> createSecurityGroup(
         String groupName, String vpcId, Optional<String> groupDescription
     ) {
 
-        Supplier<CreateSecurityGroupResult> function = () -> {
+        CreateSecurityGroupResult createSecurityGroupResult = Mockito.mock(
+            CreateSecurityGroupResult.class
+        );
+        Mockito.when(createSecurityGroupResult.getGroupId()).thenReturn("1");
 
-            CreateSecurityGroupRequest request = new CreateSecurityGroupRequest();
-            if (groupDescription.isPresent())
-                request.withGroupName(groupName).withVpcId(vpcId).withDescription(groupDescription.get());
-            else
-                request.withGroupName(groupName).withVpcId(vpcId);
-
-            CreateSecurityGroupResult createSecurityGroupResult = Mockito.mock(
-                CreateSecurityGroupResult.class
-            );
-            Mockito.when(createSecurityGroupResult.getGroupId()).thenReturn("2137139");
-
-            return createSecurityGroupResult;
-        };
-        return invokeHystrixCommand(function);
+        return Observable.just(createSecurityGroupResult);
     }
+
 
     @Override
     public Observable<Void> deleteSecurityGroup(String groupId) {
 
-        Supplier<Void> function = () -> {
-            return null;
-        };
-        return invokeHystrixCommand(function);
+        return Observable.empty();
     }
+
 
     /**
      * Private method to construct {@link IpPermission} as part of a request to
      * authorize ingress or egress to a security group.
-     * 
-     * @param fromPort the port for which access should be granted from
-     * @param toPort the port for which access should be granted to
-     * @param protocol the protocol associated with the permission
-     * @param cidr the cidr if it exists
-     * @param destinationGroupId the destinationGroupId if it exists
+     *
+     * @param fromPort           the port for which access should be granted from.
+     * @param toPort             the port for which access should be granted to.
+     * @param protocol           the protocol associated with the permission.
+     * @param cidr               the cidr if it exists.
+     * @param destinationGroupId the destinationGroupId if it exists.
      * @return {@link IpPermission}
      */
     private IpPermission constructPermission(
-        int fromPort, int toPort, String protocol, Optional<String> cidr
-        , Optional<String> destinationGroupId
+        int fromPort, int toPort, String protocol, Optional<String> cidr, Optional<String> destinationGroupId
     ) {
 
-        IpPermission permission = new IpPermission().withFromPort(fromPort).withToPort(toPort).withIpProtocol(protocol);
-        return cidr.isPresent() ? permission.withIpRanges(cidr.get()) : permission
-            .withUserIdGroupPairs(new UserIdGroupPair().withGroupId(destinationGroupId.get()));
+        IpPermission permission = new IpPermission()
+            .withFromPort(fromPort)
+            .withToPort(toPort)
+            .withIpProtocol(protocol);
+
+        if (cidr.isPresent()) {
+            return permission.withIpRanges(cidr.get());
+        }
+
+        return permission.withUserIdGroupPairs(new UserIdGroupPair().withGroupId(destinationGroupId.orElse(null)));
     }
+
 
     @Override
     public Observable<Void> createSecurityGroupEgressRule(
@@ -147,16 +169,9 @@ public class EC2ClientStub implements EC2Client {
         , Optional<String> destinationGroupId
     ) {
 
-        if (!cidr.isPresent() && !destinationGroupId.isPresent() || cidr.isPresent() && destinationGroupId.isPresent())
-            throw new IllegalArgumentException("Either a CIDR or destination security group ID must be passed");
-        Supplier<Void> function = () -> {
-            AuthorizeSecurityGroupEgressRequest request = new AuthorizeSecurityGroupEgressRequest();
-            IpPermission permission = constructPermission(fromPort, toPort, protocol, cidr, destinationGroupId);
-
-            return null;
-        };
-        return invokeHystrixCommand(function);
+        return Observable.empty();
     }
+
 
     @Override
     public Observable<Void> createSecurityGroupIngressRule(
@@ -164,16 +179,9 @@ public class EC2ClientStub implements EC2Client {
         , Optional<String> destinationGroupId
     ) {
 
-        if (!cidr.isPresent() && !destinationGroupId.isPresent() || cidr.isPresent() && destinationGroupId.isPresent())
-            throw new IllegalArgumentException("Either a CIDR or destination security group ID must be passed");
-        Supplier<Void> function = () -> {
-            AuthorizeSecurityGroupIngressRequest request = new AuthorizeSecurityGroupIngressRequest();
-            IpPermission permission = constructPermission(fromPort, toPort, protocol, cidr, destinationGroupId);
-
-            return null;
-        };
-        return invokeHystrixCommand(function);
+        return Observable.empty();
     }
+
 
     @Override
     public Observable<Void> deleteSecurityGroupIngressRule(
@@ -181,16 +189,9 @@ public class EC2ClientStub implements EC2Client {
         , Optional<String> destinationGroupId
     ) {
 
-        if (!cidr.isPresent() && !destinationGroupId.isPresent() || cidr.isPresent() && destinationGroupId.isPresent())
-            throw new IllegalArgumentException("Either a CIDR or destination security group ID must be passed");
-        Supplier<Void> function = () -> {
-            RevokeSecurityGroupIngressRequest request = new RevokeSecurityGroupIngressRequest();
-            IpPermission permission = constructPermission(fromPort, toPort, protocol, cidr, destinationGroupId);
-
-            return null;
-        };
-        return invokeHystrixCommand(function);
+        return Observable.empty();
     }
+
 
     @Override
     public Observable<Void> deleteSecurityGroupEgressRule(
@@ -198,40 +199,36 @@ public class EC2ClientStub implements EC2Client {
         , Optional<String> destinationGroupId
     ) {
 
-        if (!cidr.isPresent() && !destinationGroupId.isPresent() || cidr.isPresent() && destinationGroupId.isPresent())
-            throw new IllegalArgumentException("Either a CIDR or destination security group ID must be passed");
-        Supplier<Void> function = () -> {
-            RevokeSecurityGroupEgressRequest request = new RevokeSecurityGroupEgressRequest();
-            IpPermission permission = constructPermission(fromPort, toPort, protocol, cidr, destinationGroupId);
-
-            return null;
-        };
-        return invokeHystrixCommand(function);
+        return Observable.empty();
     }
 
+
+    /**
+     * Returns new mock groups corresponding to queried group vpc's.
+     *
+     * @param vpcs the vpc's.
+     * @return new mock groups corresponding to queried group vpc's.
+     */
     @Override
     public Observable<DescribeVpcsResult> describeVpcs(Optional<List<String>> vpcs) {
 
-        Supplier<DescribeVpcsResult> function = () -> {
-            DescribeVpcsRequest request = new DescribeVpcsRequest();
-            if (vpcs.isPresent())
-                request.withVpcIds(vpcs.get());
+        DescribeVpcsRequest request = new DescribeVpcsRequest();
+        if (vpcs.isPresent())
+            request.withVpcIds(vpcs.get());
 
-            DescribeVpcsResult describeVpcsResult = Mockito.mock(DescribeVpcsResult.class);
-            List<Vpc> vpcList = new ArrayList<>();
-            vpcs.get().forEach(vpc -> {
-                vpcList.add(new Vpc().withVpcId(vpc));
-            });
-            Mockito.when(describeVpcsResult.getVpcs()).thenReturn(vpcList);
+        DescribeVpcsResult describeVpcsResult = Mockito.mock(DescribeVpcsResult.class);
+        List<Vpc> vpcList = new ArrayList<>();
+        vpcs.get().forEach(vpc -> {
+            vpcList.add(new Vpc().withVpcId(vpc));
+        });
+        Mockito.when(describeVpcsResult.getVpcs()).thenReturn(vpcList);
 
-            return describeVpcsResult;
-        };
-        return invokeHystrixCommand(function);
+        return Observable.just(describeVpcsResult);
     }
+
 
     /**
      * Builder class for constructing an instance of {@link S3Client}
-     * 
      */
     public static class Builder {
 
@@ -243,9 +240,10 @@ public class EC2ClientStub implements EC2Client {
         private ClientConfiguration config;
         private Region region;
 
+
         /**
          * Constructor for {@link AWSAuthType}
-         * 
+         *
          * @param authType {@link AWSAuthType}
          */
         public Builder(AWSAuthType authType) {
@@ -254,9 +252,10 @@ public class EC2ClientStub implements EC2Client {
             this.config = new ClientConfiguration();
         }
 
+
         /**
          * Type of authentication used to talk to AWS
-         * 
+         *
          * @param authType
          * @return {@link Builder}
          */
@@ -266,12 +265,13 @@ public class EC2ClientStub implements EC2Client {
             return this;
         }
 
+
         /**
          * Sets the name of the profile specified in the profile config, and
          * used with an auth type of {@link AWSAuthType#PROFILE} <br />
          * <br />
          * Default value is <code>"default"</code>
-         * 
+         *
          * @param profileName
          * @return {@link Builder}
          */
@@ -281,14 +281,15 @@ public class EC2ClientStub implements EC2Client {
             return this;
         }
 
+
         /**
          * Sets the physical location of the profile config, and used with an
          * auth type of {@link AWSAuthType#PROFILE} <br />
          * <br />
-         * 
+         * <p>
          * Default behavior loads the profile config from
          * <code>~/.aws/credentials</code>
-         * 
+         *
          * @param profileConfigFilePath
          * @return {@link Builder}
          */
@@ -298,10 +299,11 @@ public class EC2ClientStub implements EC2Client {
             return this;
         }
 
+
         /**
          * Sets the {@link ClientConfiguration} used to configure the
          * {@link AmazonS3Client}
-         * 
+         *
          * @param config {@link ClientConfiguration}
          * @return {@link Builder}
          */
@@ -311,10 +313,11 @@ public class EC2ClientStub implements EC2Client {
             return this;
         }
 
+
         /**
          * Sets the AWS Account Key used to configure the
          * {@link AmazonEC2Client}
-         * 
+         *
          * @param awsAccountKey {@code awsAccountKey}
          * @return {@link Builder}
          */
@@ -324,9 +327,10 @@ public class EC2ClientStub implements EC2Client {
             return this;
         }
 
+
         /**
          * Sets the AWS Secret Key used to configure the {@link AmazonEC2Client}
-         * 
+         *
          * @param awsSecretKey {@code awsSecretKey}
          * @return {@link Builder}
          */
@@ -336,9 +340,10 @@ public class EC2ClientStub implements EC2Client {
             return this;
         }
 
+
         /**
          * Sets the Region uses to configure the {@link AmazonEC2Client}
-         * 
+         *
          * @param region
          * @return {@link Builder}
          */
@@ -348,35 +353,12 @@ public class EC2ClientStub implements EC2Client {
             return this;
         }
 
+
         public EC2Client build() {
 
-            EC2ClientStub client = null;
-            if (this.authType == AWSAuthType.CREDENTIALS && awsAccountKey != null && awsSecretKey != null) {
-                client = new EC2ClientStub();
-            }
-
-            if (this.authType == AWSAuthType.PROFILE && profileConfigFilePath == null && profileName != null) {
-                client = new EC2ClientStub();
-            }
-
-            if (this.authType == AWSAuthType.PROFILE && profileConfigFilePath != null && profileName != null) {
-                client = new EC2ClientStub();
-            }
-
-            if (this.authType == AWSAuthType.INSTANCE_ROLE) {
-                client = new EC2ClientStub();
-            }
-
-            if (client != null && region != null) {
-                return client;
-            }
-
-            if (client != null) {
-                return client;
-            }
-
-            throw new IllegalStateException("Invalid S3Client configuration");
+            return new EC2ClientStub();
         }
+
 
         /**
          * Creates a {@code ClientConfiguration} object using the System
@@ -385,9 +367,9 @@ public class EC2ClientStub implements EC2Client {
          * {@code -Dhttp.proxyHost=my.proxy.host.com -Dhttp.proxyPort=3128} and
          * if auth is required
          * {@code -Dhttp.proxyUser=username -Dhttp.proxyPassword=password1234}.
-         * 
+         *
          * @return A {@ClientConfiguration}. Never
-         *         {@code null}.
+         * {@code null}.
          */
         public ClientConfiguration getConfiguration() {
 
